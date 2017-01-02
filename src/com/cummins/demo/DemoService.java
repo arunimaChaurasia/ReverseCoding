8359357f0a13bd51aab16181d8ca4e7d0c980d38
adi2120 <aditi.sharma@cumminscollege.in>
2017-01-02 21:40:26 +0530

    Update DemoService.java
    with response and request

--- src/com/cummins/demo/DemoService.java
+++ src/com/cummins/demo/DemoService.java
@@ -8,6 +8,7 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,26 +18,12 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 //@EnableWebMvc
 
 public class DemoService {
-
-	
-	
 	 
-	 @RequestMapping( value="/sectorDisplay",method=RequestMethod.GET)//,produces="application/json")
-
-
-	 public 	 @ResponseBody String getvalue()
+	 @RequestMapping( value="/sectorDisplay",method=RequestMethod.GET,produces="application/json")
+	 public @ResponseBody String getSector()
 	 {
 		 SectorDB sector=new SectorDB();
 		 ObjectMapper mapper = new ObjectMapper();
-
-
-		 //Object to JSON in file
-		 try {
-			mapper.writeValue(new File("file.json"), sector.returnJson());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
 		 //Object to JSON in String
 		 String jsonInString=new String();
@@ -48,6 +35,33 @@ public class DemoService {
 		}
 		System.out.println("service called");
 		 return jsonInString;
+	 }
+	 
+	 @RequestMapping( value="/securityDisplay",method=RequestMethod.POST,produces="application/json",consumes="application/json")
+	 public @ResponseBody String getSecurity(@RequestBody Sector sector )
+	 {
+		
+		if(sector.getSymbol()=="auto")
+		{
+			 SecurityDB securitydb=new SecurityDB();
+		
+		 ObjectMapper mapper = new ObjectMapper();
+
+		 //Object to JSON in String
+		 String jsonInString=new String();
+		try {
+			jsonInString = mapper.writeValueAsString(securitydb.returnJson());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("service return called");
+		 return jsonInString;
+		}
+		else
+		{
+			return "not valid value";
+		}
 	 }
 	 
 	
