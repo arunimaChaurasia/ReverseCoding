f35979eff699ee57885f50121c181a65830fb242
aditi <aditi@192.168.2.8>
2017-01-10 19:00:29 +0530

    commit

---
+++ src/com/cummins/testPackage/GetterSetter.java
@@ -0,0 +1,39 @@
+package com.cummins.testPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.cummins.Stocks.Stock;
+import com.cummins.UserDetails.AccountDetail;
+import com.cummins.UserDetails.Userdetail;
+
+public class GetterSetter{
+
+	@Test
+	    public void testSetOpen() {
+	        Stock stock = new Stock();
+	        stock.setOpen("134.14");
+	        assertTrue(stock.getOpen() == "134.14");
+	    }
+	@Test
+		public void testSetEps(){
+		Stock stock = new Stock();
+        stock.setEps("587.73");
+        assertTrue(stock.getEps() == "587.73");
+	}
+	@Test
+	public void testSetSecurityCode(){
+		AccountDetail accountdetail=new AccountDetail();
+		accountdetail.setSecurity_code("BHEL");
+		 assertTrue(accountdetail.getSecurity_code() == "BHEL");
+		}
+	@Test
+	public void testSetDetailUser(){
+		Userdetail userdetail=new Userdetail();
+		userdetail.setFirst_name("Sakshi");
+		 assertTrue(userdetail.getFirst_name() == "Sakshi");
+		}
+	
+
+}
