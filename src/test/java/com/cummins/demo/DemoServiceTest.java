8111f5d4faf315e7301ba0d37fd8c1c62fda0baa
Agatha Chatterjee <Agatha Chatterjee@192.168.2.11>
2017-01-21 04:20:33 +0530

    updated test cases

--- src/test/java/com/cummins/demo/DemoServiceTest.java
+++ src/test/java/com/cummins/demo/DemoServiceTest.java
@@ -1,10 +1,13 @@
-/*package com.cummins.demo;
+package com.cummins.demo;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.IOException;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -25,6 +28,8 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.cummins.Stocks.Sector;
+import com.cummins.UserDetails.Userdetail;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
@@ -50,8 +55,71 @@ public class DemoServiceTest {
 	    }
 	    @Test
 	    public void displaysecurity() throws Exception {
-	    	
+	    	Sector sector=new Sector();
+	    	sector.setName("Media And Entertainment");
+	    	sector.setSymbol("MEDIA");
+	    	ObjectMapper mapper = new ObjectMapper();
+
+			// Object to JSON in String
+			String jsonInString = new String();
+			try {
+				jsonInString = mapper.writeValueAsString(sector);
+				// jsonInSell= mapper.writeValueAsString(predictStocks.)
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    	  mockMvc.perform(post("/securityDisplay",jsonInString).accept(MediaType.APPLICATION_JSON))
+	    	         .andExpect(status().isOk())
+	    	         .andExpect(content().contentType("application/json;charset=UTF-8"))
+	    	         .andExpect(jsonPath("symbol").value("ZEEL"));
 	    }
+	    @Test
+	    public void logintest() throws Exception {
+	    	Userdetail userdetail=new Userdetail();
+	    	userdetail.setEmail_id("harrypotter@hogwarts.com");
+	    	userdetail.setPassword("Magic@1Maker");
+	    	ObjectMapper mapper = new ObjectMapper();
+
+			// Object to JSON in String
+			String jsonInString = new String();
+			try {
+				jsonInString = mapper.writeValueAsString(userdetail);
+				// jsonInSell= mapper.writeValueAsString(predictStocks.)
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    	  mockMvc.perform(post("/Login",jsonInString).accept(MediaType.APPLICATION_JSON))
+	    	         .andExpect(status().isOk())
+	    	         .andExpect(content().contentType("application/json;charset=UTF-8"))
+	    	         .andExpect(jsonPath("password").value("Magic@1Maker"));
+	    }
+	    @Test
+	    public void logintestNotOk() throws Exception {
+	    	Userdetail userdetail=new Userdetail();
+	    	userdetail.setEmail_id("harrypotter@hogwarts.com");
+	    	userdetail.setPassword("harryPotter");
+	    	ObjectMapper mapper = new ObjectMapper();
+
+			// Object to JSON in String
+			String jsonInString = new String();
+			try {
+				jsonInString = mapper.writeValueAsString(userdetail);
+				// jsonInSell= mapper.writeValueAsString(predictStocks.)
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    	  mockMvc.perform(post("/Login",jsonInString).accept(MediaType.APPLICATION_JSON))
+	    	         .andExpect(status().isOk())
+	    	         .andExpect(content().contentType("application/json;charset=UTF-8"))
+	    	         .andExpect(jsonPath("password").value("Invalid UserName or Password"));
+	    }
+	    
 	 
 	    @Configuration
 	    @EnableWebMvc
@@ -64,4 +132,3 @@ public class DemoServiceTest {
 	 
 	    }
 	}
-*/
